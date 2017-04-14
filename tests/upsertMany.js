a320df2ef59b152934e93f3a82cd10087b6d41a0
Francesco Bianchi <f.bianchi@palmabit.com>
2017-04-14 17:11:04 +0200

    add upsertMany

---
+++ tests/upsertMany.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const MongoClient = require('mongodb').MongoClient
+
+const chai = require('chai')
+const expect = chai.expect
+const assert = chai.assert
+const chaiSubset = require('chai-subset')
+
+chai.use(chaiSubset)
+chai.should()
+
+const stringConnection = 'mongodb://localhost:27017/lambda-mongo-crud-test'
+const tableName = 'posts'
+
+const Crud = require('./../lib/crud')
+
+const PERMISSION = {
+  list: 'posts:list',
+  get: 'posts:get',
+  save: 'posts:save',
+  delete: 'posts:delete'
+}
+
+const ROLES = {
+  admin: {
+    can: [PERMISSION.save, PERMISSION.delete, PERMISSION.list, PERMISSION.get]
+  }
+}
+
+describe('MANY', () => {
+
+  beforeEach((done) => {
+
+    MongoClient.connect(stringConnection, (err, db) => {
+
+      let collection = db.collection(tableName)
+      collection.insertMany([{ title: "Title 1", code: 1 }, { title: "Title 2", code: 2 }, { title: "Title 3", code: 3 }])
+      db.close()
+      done()
+
+    })
+  })
+
+  after((done) => {
+    MongoClient.connect(stringConnection, (err, db) => {
+
+      let collection = db.collection(tableName)
+      collection.remove()
+      db.close()
+      done()
+
+    })
+  })
+
+  const crud = new Crud(stringConnection, tableName, { role: 'admin', _id: 123 }, ROLES)
+
+  describe('list()', () => {
+
+    it('should return an array', (done) => {
+      crud.list({}, PERMISSION.list, {}, (err, docs) => {
+        expect(docs.length).to.be.equal(3)
+        done()
+      })
+    })
+
+    it('should return an array', (done) => {
+
+      const data = [
+        { title: "Title 3bis", code: 3 },
+        { title: "Title 4", code: 4 },
+        { title: "Title 5", code: 5 }
+      ]
+
+      crud.upsertMany(data, "code", PERMISSION.save, {}, (err, result) => {
+        expect(result.nInserted).to.be.equal(3)
+        expect(result.nUpserted).to.be.equal(1)
+        done()
+      })
+    })
+  })
+
+})
+
